6f7aa58ff2dcb2e7ca9e463b05f60eafa86ba81c
Ryan Morales <rymorale@adobe.com>
2020-04-28 23:28:27 -0700

    Add ACPCore Cocoapod xamarin binding
    - includes ACPCore, ACPIdentity, ACPSignal, and ACPLifecycle.

---
+++ src/Adobe.ACPCore.iOS/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+using Foundation;
+
+// This attribute allows you to mark your assemblies as “safe to link”.
+// When the attribute is present, the linker—if enabled—will process the assembly
+// even if you’re using the “Link SDK assemblies only” option, which is the default for device builds.
+
+[assembly: LinkerSafe]
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("ACPCore for Xamarin iOS")]
+[assembly: AssemblyDescription("ACPCore for Xamarin iOS")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Adobe Inc.")]
+[assembly: AssemblyProduct("ACPCore")]
+[assembly: AssemblyCopyright("Copyright 2020 Adobe All Rights Reserved. NOTICE: Adobe permits you to use, modify, and distribute this file in accordance with the terms of the Adobe license agreement accompanying it.If you have received this file from a source other than Adobe, then your use, modification, or distribution of it requires the prior written permission of Adobe. This file has been modified from its original form.The original license can be viewed in the NOTICES.txt file.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+//      Major Version
+//      Minor Version 
+//      Patch Version
+[assembly: AssemblyVersion("2.6.1")]
+
+// The following attributes are used to specify the signing key for the assembly,
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
