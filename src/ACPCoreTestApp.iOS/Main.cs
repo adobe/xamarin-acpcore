6120dbe003e97bcbf4053c1ab5ca6b4c938c0ccb
Ryan Morales <rymorale@adobe.com>
2020-05-12 14:07:31 -0700

    add copyright headers

--- src/ACPCoreTestApp.iOS/Main.cs
+++ src/ACPCoreTestApp.iOS/Main.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/*
+    Copyright 2020 Adobe
+    All Rights Reserved.
+    NOTICE: Adobe permits you to use, modify, and distribute this file in
+    accordance with the terms of the Adobe license agreement accompanying
+    it. If you have received this file from a source other than Adobe,
+    then your use, modification, or distribution of it requires the prior
+    written permission of Adobe.
+    This file has been modified from its original form. The original
+    license can be viewed in the NOTICES.txt file.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
