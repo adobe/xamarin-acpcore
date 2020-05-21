f564e3b1f73deb27d6011e9fa3052aac90aa5a83
Ryan Morales <rymorale@adobe.com>
2020-05-21 15:05:39 -0700

    move MIT license to sample app directories
    - also update notices.txt with information regarding the license used for the sample apps.

--- src/ACPCoreTestApp.iOS/Properties/AssemblyInfo.cs
+++ src/ACPCoreTestApp.iOS/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ NOTICE: Adobe permits you to use, modify, and distribute this file in
 accordance with the terms of the Adobe license agreement accompanying
 it. If you have received this file from a source other than Adobe,
 then your use, modification, or distribution of it requires the prior
-written permission of Adobe. (See LICENSE-MIT in the root folder for details)
+written permission of Adobe. (See LICENSE-MIT for details)
 */
 
 using System.Reflection;
